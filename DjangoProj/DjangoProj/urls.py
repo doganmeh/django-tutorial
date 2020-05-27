5a773865e518ef71789be4f324922c1d26821c1d
mehmet <mehmet@edgle.com>
2020-05-26 21:48:59 -0500

    redirect empty url to /polls/question/list/

--- DjangoProj/DjangoProj/urls.py
+++ DjangoProj/DjangoProj/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/polls/question/list/'), ),
     path('polls/', include('polls.urls', namespace='polls')),
     path('admin/', admin.site.urls),
 ]
